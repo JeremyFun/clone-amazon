4a1581c59457795c78bdf094c6f8e4e281d4047b
Lexun <59420596+lexun1060@users.noreply.github.com>
2021-01-29 13:58:30 +0200

    add header

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAzGc_hXbR6hxS_VWcREuxNqabdPGlIVWI",
+    authDomain: "clone-8e15c.firebaseapp.com",
+    projectId: "clone-8e15c",
+    storageBucket: "clone-8e15c.appspot.com",
+    messagingSenderId: "776810459213",
+    appId: "1:776810459213:web:ef83b135b6b9669060ec04",
+    measurementId: "G-HDBD6VLK3M"
+};
